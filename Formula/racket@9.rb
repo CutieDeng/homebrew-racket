ec353b9c801b090036df3997b460e3543f49100e
Cutie Deng <cutiedeng@icloud.com>
2026-06-20 03:39:56 +0800

    (FEAT "Add racket@9 formula and update README"
    "
    Add Racket 9.2.1 private build formula (keg-only, versioned) with
    Homebrew OpenSSL 3 integration and raco setup.
    
    Modified:
    - .gitignore
    - Formula/racket@9.rb
    - README.md
    ")

---
+++ Formula/racket@9.rb
@@ -0,0 +1,111 @@
+class RacketAT9 < Formula
+  desc "Modern programming language in the Lisp/Scheme family"
+  homepage "https://racket-lang.org/"
+  url "https://github.com/CutieDeng/racket/releases/download/v9.2.1/racket-minimal-9.2.1-src.tgz"
+  sha256 "8a694ec122c9d6ac1a219fa18f43a590e45eaa792d2da22872c2eefafe0fc0ee"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  livecheck do
+    skip "Private Racket fork releases are managed manually"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "openssl@3"
+
+  uses_from_macos "libffi"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
+  # These files are amended when packages are installed or removed.
+  skip_clean "lib/racket/launchers.rktd", "lib/racket/mans.rktd"
+
+  def racket_config
+    etc/"racket/config.rktd"
+  end
+
+  def install
+    # Configure racket's package tool (raco) to use installation scope.
+    inreplace "etc/config.rktd", /\)\)\n$/, ") (default-scope . \"installation\"))\n"
+
+    # Prefer Homebrew OpenSSL 3 over older OpenSSL variants.
+    inreplace %w[libssl.rkt libcrypto.rkt].map { |file| buildpath/"collects/openssl"/file },
+              '"1.1"', '"3"'
+
+    cd "src" do
+      args = %W[
+        --disable-debug
+        --disable-dependency-tracking
+        --enable-origtree=no
+        --enable-macprefix
+        --prefix=#{prefix}
+        --mandir=#{man}
+        --sysconfdir=#{etc}
+        --enable-useprefix
+      ]
+
+      ENV["LDFLAGS"] = "-rpath #{Formula["openssl@3"].opt_lib}"
+      ENV["LDFLAGS"] = "-Wl,-rpath=#{Formula["openssl@3"].opt_lib}" if OS.linux?
+
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+
+      if OS.mac?
+        openssl = Formula["openssl@3"]
+        racket_libdir = lib/"racket"
+
+        %w[libssl.3.dylib libcrypto.3.dylib].each do |dylib|
+          path = racket_libdir/dylib
+          path.unlink if path.exist?
+        end
+
+        ln_s openssl.opt_lib/"libssl.3.dylib",    racket_libdir/"libssl.3.dylib"
+        ln_s openssl.opt_lib/"libcrypto.3.dylib", racket_libdir/"libcrypto.3.dylib"
+      end
+    end
+
+    inreplace racket_config, prefix, opt_prefix
+  end
+
+  def post_install
+    system bin/"raco", "setup"
+
+    return unless racket_config.read.include?(HOMEBREW_CELLAR)
+
+    ohai "Fixing up Cellar references in #{racket_config}..."
+    inreplace racket_config, %r{#{Regexp.escape(HOMEBREW_CELLAR)}/racket@9/[^/]+}o, opt_prefix
+  end
+
+  def caveats
+    <<~EOS
+      This is a versioned private Racket build. It is keg-only and will not
+      replace an existing `racket` or `raco` on PATH automatically.
+
+      To use this build directly:
+        #{opt_bin}/racket
+        #{opt_bin}/raco
+    EOS
+  end
+
+  test do
+    assert_match "9.2.1", shell_output("#{bin}/racket -e '(displayln (version))'")
+
+    output = shell_output("#{bin}/racket -e '(require racket/pvector) (displayln (pvector->list (pvector 1 2 3)))'")
+    assert_match "(1 2 3)", output
+
+    output = shell_output("'#{bin}/raco' pkg config")
+    assert $CHILD_STATUS.success?
+    assert_match "default-scope:\n installation", output
+
+    if OS.mac?
+      output = shell_output("DYLD_PRINT_LIBRARIES=1 #{bin}/racket -e '(require openssl)' 2>&1")
+      assert_match(%r{.*openssl@3/.*/libssl.*\.dylib}, output)
+    else
+      output = shell_output("LD_DEBUG=libs #{bin}/racket -e '(require openssl)' 2>&1")
+      assert_match "init: #{Formula["openssl@3"].opt_lib/shared_library("libssl")}", output
+    end
+  end
+end
--- README.md
+++ README.md
@@ -1,16 +1,54 @@
-# CutieDeng Racket
+# CutieDeng Racket Homebrew Tap
+
+This tap packages private Racket builds maintained from
+`https://github.com/CutieDeng/racket`.
 
 ## How do I install these formulae?
 
-`brew install cutiedeng/racket/<formula>`
+Install the private Racket 9 build directly:
 
-Or `brew tap cutiedeng/racket` and then `brew install <formula>`.
+```sh
+brew install cutiedeng/racket/racket@9
+```
+
+Or tap first:
+
+```sh
+brew tap cutiedeng/racket
+brew install racket@9
+```
 
 Or, in a `brew bundle` `Brewfile`:
 
 ```ruby
 tap "cutiedeng/racket"
-brew "<formula>"
+brew "racket@9"
+```
+
+`racket@9` is keg-only, so it does not replace another `racket` on PATH
+automatically. Use `/opt/homebrew/opt/racket@9/bin/racket` directly or add
+that directory to PATH when this build should be the active Racket.
+
+## Release Checklist
+
+Before committing `Formula/racket@9.rb`, upload this source artifact:
+
+```text
+https://github.com/CutieDeng/racket/releases/download/v9.2.1/racket-minimal-9.2.1-src.tgz
+```
+
+Then verify that the formula `sha256` matches:
+
+```sh
+curl -L https://github.com/CutieDeng/racket/releases/download/v9.2.1/racket-minimal-9.2.1-src.tgz | shasum -a 256
+```
+
+Local validation:
+
+```sh
+brew audit --strict --formula cutiedeng/racket/racket@9
+brew install --build-from-source --verbose cutiedeng/racket/racket@9
+brew test cutiedeng/racket/racket@9
 ```
 
 ## Documentation
