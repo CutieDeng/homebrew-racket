13dcef5371ba8189b970cf922354852dff932190
Cutie Deng <cutiedeng@icloud.com>
2026-06-23 12:34:21 +0800

    Fix Homebrew formula style

--- Formula/racket@9.rb
+++ Formula/racket@9.rb
@@ -102,7 +102,6 @@ class RacketAT9 < Formula
 
     assert_match "9.2.1", shell_output("#{bin}/racket -e '(displayln (version))'")
 
-
     output = shell_output("#{bin}/racket -e '(require racket/pvector) (displayln (pvector->list (pvector 1 2 3)))'")
     assert_match "(1 2 3)", output
 
